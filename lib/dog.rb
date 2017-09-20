2e77f4c60788c34c144c4a4ef7df393c9d4b780b
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2017-09-20 02:26:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,8 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed()
-    @breed
   end
 
 end
 
-fido = Dog.new("Fido", breed = "Mutt")
-fido.name
-fido.breed
